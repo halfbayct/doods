72e7397097f145667718652923e1de39897fbb89
halfbayct <104414722+halfbayct@users.noreply.github.com>
2022-09-10 01:06:37 -0400

    🚀🎫

--- bot.js
+++ bot.js
@@ -16,7 +16,13 @@ Steps to use:
   // only need to load count once
   let totalDoods = 0;
 
+  const authentication = {
+    username: "",
+    password: "",
+  };
+
   const startTime = new Date();
+  let totalRefreshes = 0;
   let tickDate = new Date();
   let expires = new Date(new Date() + 12 * 60 * 60 * 1000);
 
@@ -33,7 +39,31 @@ Steps to use:
   header.style = styles.join(";");
   const timerElement = document.createElement("p");
   const lastTickElement = document.createElement("p");
-  
+
+  const elements = {
+    login: {
+      username: () => document.querySelector('.login-container input[type="text"]'),
+      password: () => document.querySelector('.login-container input[type="password"]'),
+      button: () => document.querySelector(".login-container .input-button"),
+    },
+    nav: {
+      available: () => document.querySelector(".available"),
+      inProgress: () => document.querySelector(".current"),
+      completed: () => document.querySelector(".completed"),
+    },
+    doodCount: () => document.querySelector(".balance-amount"),
+    toastBody: () => document.querySelector(".Toastify__toast-body"),
+    sendAllButton: () => document.querySelector(".send-mission-button"),
+    sendMissionGoBack: () => document.querySelector(".send-mission-go-back"),
+    missionCompletedButton: () => document.querySelector(".mission-send button"),
+    homeButton: () => document.querySelector(".launch-game button"),
+    questButton: () => document.querySelectorAll(".mission-send button")[3],
+  };
+
+  let captured = null;
+  addXMLRequestCallback(function (xhr) {
+    captured = captured || xhr;
+  });
 
   function addXMLRequestCallback(callback) {
     var oldSend, i;
@@ -57,6 +87,30 @@ Steps to use:
     });
   }
 
+  async function waitForToast(msg) {
+    let shouldStop = false;
+    let watchDog = setTimeout(() => (shouldStop = true), 30000);
+    let toastText = undefined;
+    do {
+      toastText = elements.toastBody()?.textContent;
+      if (toastText) {
+        console.log("toastText:", toastText);
+      }
+      if (
+        (toastText && !msg) ||
+        (toastText && toastText.toLowerCase() === msg.toLowerCase())
+      ) {
+        clearTimeout(watchDog);
+        shouldStop = true;
+        elements.toastBody()?.click();
+      }
+
+      await sleep(250);
+    } while (shouldStop === false);
+
+    elements.toastBody()?.click();
+  }
+
   function parseJwt(token) {
     var base64Url = token.split(".")[1];
     var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
@@ -74,17 +128,15 @@ Steps to use:
   }
 
   async function bootstrap() {
-    let captured = null;
-    addXMLRequestCallback(function (xhr) {
-      captured = captured || xhr;
-    });
+    authentication.username = elements.login.username().value;
+    authentication.password = elements.login.password().value;
 
-    document.querySelector(".input-button")?.click();
-    await sleep(2500);
+    console.log(`Credentials captured for ${authentication.username}`);
 
-    totalDoods = parseInt(
-      document.querySelector(".balance-amount").textContent
-    );
+    elements.login.button()?.click();
+    await waitForToast("Successfully logged in");
+
+    totalDoods = parseInt(elements.doodCount().textContent);
 
     header.innerHTML = `<p>BONK in SPACE enabled. Running with <b>${totalDoods}</b> characters</p>`;
     header.append(timerElement);
@@ -100,7 +152,9 @@ Steps to use:
       return;
     }
 
-    const token = (captured.getResponseHeader('authorization') || "").split(' ')[1];
+    const token = (captured.getResponseHeader("authorization") || "").split(
+      " "
+    )[1];
 
     if (!token) {
       return;
@@ -112,38 +166,91 @@ Steps to use:
 
   async function sendDoods() {
     try {
-      // clear any toast messages
-      document.querySelector('.Toastify__toast')?.click();
-
-      const sendAllButton = document.querySelector(
-        ".send-mission-button"
-      );
-      sendAllButton.click();
-      await sleep(500);
-
-      while (!document.querySelector('.Toastify__toast')) {
-        
-        await sleep(200);
-      }
-      document.querySelector('.send-mission-go-back').click();
+      elements.sendAllButton().click();
+      await waitForToast("All sent successfully!");
+      elements.sendMissionGoBack().click();
+      await sleep(250);
     } catch (e) {
       console.log(e);
-      document.querySelector(".MuiBackdrop-root")?.click();
-      document.querySelector(".send-mission-title button")?.click();
+      elements.sendMissionGoBack()?.click();
     }
   }
 
   async function completeMissions() {
     let missionCompleteBtn;
     do {
-      missionCompleteBtn = document.querySelector(".mission-send button");
+      missionCompleteBtn = elements.missionCompletedButton();
       if (!missionCompleteBtn) {
         continue;
       }
 
       missionCompleteBtn.click();
-      await sleep(150);
+      await sleep(250);
     } while (missionCompleteBtn);
+
+    await waitForToast();
+  }
+
+  function setValue(input, value) {
+    var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
+    nativeInputValueSetter.call(input, value);
+    var event = new Event('input', { bubbles: true});
+    input.dispatchEvent(event);
+  }
+
+  async function loginAgain() {
+    if (!authentication.username || !authentication.password) {
+      console.log("Unable to relog");
+      return;
+    }
+    console.log("Relog cause tokens are short... rip");
+    // const internalRoot = Array.from(document.querySelectorAll("*[id]")).find((el) => el?._reactRootContainer?._internalRoot?.current)?._reactRootContainer?._internalRoot?.current;
+    // internalRoot.memoizedState.element.props.children.props.store.getState().store.loggedIn;
+
+
+    // go home
+    elements.homeButton().click();
+    await sleep(1000);
+
+    // because the redux store does not set the login boolean correct, we need to own redux rq
+    const store = getReduxStore();
+    const prevGetState = store.getState; 
+    store.getState = function() { const state = JSON.parse(JSON.stringify(prevGetState())); state.store.loggedIn = false; console.log(state);  return state; }
+
+    // launch game
+    elements.homeButton().click();
+    await sleep(1000);
+    // recapture token
+    // input creds and login
+    captured = null;
+    setValue(elements.login.username(), authentication.username);
+    setValue(elements.login.password(), authentication.password);
+    await sleep(150);
+
+    // put redux back nicely, dont mind me...
+    store.getState = prevGetState;
+    elements.login.button().click();
+    await waitForToast("Successfully logged in");
+
+    if (!captured) {
+      console.log("relog not captured");
+      return;
+    }
+
+    const token = (captured.getResponseHeader("authorization") || "").split(
+      " "
+    )[1];
+
+    console.log("token:", token);
+
+    if (!token) {
+      return;
+    }
+
+    const payload = parseJwt(token);
+    expires = new Date(payload.exp * 1000);
+    console.log("expires:", expires);
+    await sleep(2000);
   }
 
   async function tick() {
@@ -151,56 +258,59 @@ Steps to use:
       console.log("Another tick is running");
       return;
     }
+
     lock = true;
 
     try {
+      // need to re-login
+      if (new Date().getTime() > expires.getTime()) {
+        await loginAgain();
+      }
       tickDate = new Date();
       console.log(`======== Tick @ ${tickDate.toISOString()} ========`);
 
-      const availableButton = document.querySelector(
-        ".mission-nav-button.available"
-      );
-      const completedButton = document.querySelector(
-        ".mission-nav-button.completed"
-      );
-      const inProgressButton = document.querySelector(
-        ".mission-nav-button.current"
-      );
-
       console.log(`Total doods: ${totalDoods}`);
 
-      availableButton.click();
+      elements.nav.available().click();
       await sleep(250);
-      inProgressButton.click();
+      elements.nav.inProgress().click();
       await sleep(250);
 
       const totalCompleted = parseInt(
-        completedButton.textContent.toLowerCase().replace(/\D/g, "").trim()
+        elements.nav
+          .completed()
+          .textContent.toLowerCase()
+          .replace(/\D/g, "")
+          .trim()
       );
       console.log(`Completed: ${totalCompleted}`);
 
       const totalInProgress = parseInt(
-        inProgressButton.textContent.toLowerCase().replace(/\D/g, "").trim()
+        elements.nav
+          .inProgress()
+          .textContent.toLowerCase()
+          .replace(/\D/g, "")
+          .trim()
       );
       console.log(`In Progress: ${totalInProgress}`);
 
       if (totalCompleted > 0) {
         console.log(`Triggering completed quests`);
-        completedButton.click();
+        elements.nav.completed().click();
         await sleep(250);
         await completeMissions();
       }
 
       if (totalInProgress < totalDoods) {
         console.log(`Triggering sending doods on quests`);
-        availableButton.click();
+        elements.nav.available().click();
         await sleep(500);
-        document.querySelectorAll(".mission-send button")[3].click();
-        await sleep(1000);
+        elements.questButton().click();
+        await sleep(2000);
         await sendDoods();
       }
 
-      document.querySelector(".mission-nav-button.current")?.click();
+      elements.nav.inProgress()?.click();
 
       lock = false;
       console.log(`======== END Tick @ ${tickDate.toISOString()} ========`);
@@ -208,6 +318,12 @@ Steps to use:
       console.log(e);
       lock = false;
     }
+  }
+
+  function getReduxStore() {
+    const internalRoot = Array.from(document.querySelectorAll("*[id]")).find((el) => el?._reactRootContainer?._internalRoot?.current)?._reactRootContainer?._internalRoot?.current;
+    const store = internalRoot.memoizedState.element.props.children.props.store;
+    return store;
   }
 
   function timespanText(date1, date2) {
@@ -234,7 +350,10 @@ Steps to use:
 
   function updateRuntime() {
     const now = new Date();
-    const runtime = now.getTime() > expires.getTime() ? 'EXPIRED' : timespanText(expires, now);
+    const runtime =
+      now.getTime() > expires.getTime()
+        ? "EXPIRED"
+        : timespanText(expires, now);
 
     let html = `Token Expires: ${runtime}`;
     timerElement.innerHTML = html;
@@ -247,9 +366,9 @@ Steps to use:
   setInterval(updateRuntime, 1000);
   // set up ticks
   bootstrap()
+    .then(() => sleep(2000))
     .then(() => tick())
     .then(async () => {
-      await sleep(15 * 1000);
       setInterval(tick, 60 * 1000);
     });
 })();
